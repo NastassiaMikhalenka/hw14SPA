2cabd8ad21127583c5381a190f805ce2b91ddb1d
NastassiaMikhalenka <72052820+NastassiaMikhalenka@users.noreply.github.com>
2021-02-26 04:20:55 +0300

    d

---
+++ SPA_HW/index.js
@@ -0,0 +1,21 @@
+import App from './components/app.js';
+import {header} from './components/header.js';
+import './components/nav.js';
+import {main} from './components/main.js';
+import {footer} from './components/footer.js';
+import localStorageProducts from './localStorage.js'
+
+
+
+    // const i = document.querySelectorAll('span')
+    
+    // const sum = 0 
+    // i.forEach(span =>{
+    //     sum =+ isNan(span.innerHTML)
+    // })
+    // console.log(sum)
+    
+    // .textContent;
+    //const i = document.querySelector('span').innerHTML;
+    // console.log(i)
+    
