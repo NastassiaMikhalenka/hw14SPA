8aab8928c8a09c0ac3a05e1ed51fbda6fd8d0003
NastassiaMikhalenka <72052820+NastassiaMikhalenka@users.noreply.github.com>
2021-03-07 18:54:00 +0300

    Update index.js

--- SPA_HW/index.js
+++ SPA_HW/index.js
@@ -6,16 +6,3 @@ import {footer} from './components/footer.js';
 import localStorageProducts from './localStorage.js'
 
 
-
-    // const i = document.querySelectorAll('span')
-    
-    // const sum = 0 
-    // i.forEach(span =>{
-    //     sum =+ isNan(span.innerHTML)
-    // })
-    // console.log(sum)
-    
-    // .textContent;
-    //const i = document.querySelector('span').innerHTML;
-    // console.log(i)
-    
